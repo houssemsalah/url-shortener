a7b401a9d6d80c34c6ce10fbc9eab9b6edf2f804
houssemsalah <houssemsalah10@gmail.com>
2025-02-07 22:10:40 +0100

    vercel config

--- url-shortener-backend/src/main.ts
+++ url-shortener-backend/src/main.ts
@@ -2,17 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-
+import { ExpressAdapter } from '@nestjs/platform-express';
+import  express from 'express';
 import 'reflect-metadata';
 
+const server = express();
 
-// Create an Express instance
+export const createNestApp = async (expressInstance: express.Express) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
 
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  
-   const config = new DocumentBuilder()
+  // Swagger configuration
+  const config = new DocumentBuilder()
     .setTitle('URL Shortener API')
     .setDescription('API for shortening URLs and tracking analytics')
     .setVersion('1.0')
@@ -21,23 +24,39 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
+
   // Enable CORS
   app.enableCors({
-    origin: [
-      'https://url-shortener-frontend-omega-lovat.vercel.app', // Frontend production URL
-      'http://localhost:3000', // Local development URL
-    ],
-    methods: 'GET, POST, PUT, DELETE, OPTIONS',
-    allowedHeaders: 'Content-Type, Authorization',
-    credentials: true, // Allows cookies, if needed
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000', // Make this configurable
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
   });
-  // Enable validation
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    transform: true,
-  }));
-  await app.listen(3001);
- 
-}
-bootstrap();
 
+  // Enable validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  return app;
+};
+
+async function bootstrap() {
+  const app = await createNestApp(server);
+  await app.init();
+  
+  // Only listen to port when running in development
+  if (process.env.NODE_ENV !== 'production') {
+    await app.listen(process.env.PORT || 3001);
+  }
+}
+
+// Local development
+if (process.env.NODE_ENV !== 'production') {
+  bootstrap();
+}
+
+// Export for serverless use
+export default server;
